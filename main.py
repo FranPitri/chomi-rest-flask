7f1fe26fce502b3c6a591e5e4cced40d171346c8
Unknown <franpitri@gmail.com>
2017-10-26 22:27:01 -0300

    wrap everything into main so that it works

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import sys
 from flask import Flask, jsonify, render_template, request
-from models import db, Track
-from schemas import ma, tracks_schema
-import uuid
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:toor@172.17.0.1/restest'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:toor@172.17.0.1/existingrestest'
 
-db.init_app(app)
-ma.init_app(app)
+db = SQLAlchemy(app)
+db.Model.metadata.reflect(db.engine)
 
+ma = Marshmallow(app)
+
+###################### MODELS ########################
+
+
+class Track(db.Model):
+    __table__ = db.Model.metadata.tables['Track']
+
+
+class TrackSchema(ma.ModelSchema):
+    class Meta:
+        model = Track
+
+track_schema = TrackSchema()
+tracks_schema = TrackSchema(many=True)
+
+
+###################### VIEWS ########################
 
 @app.route('/api/tracks/', methods=['GET'])
 def list_tracks():
@@ -34,9 +51,9 @@ if __name__ == '__main__':
 
     elif 'seeddb' in sys.argv:
         with app.app_context():
-            t1 = Track(id=uuid.uuid4(), name="Never Hit Bro")
-            t2 = Track(id=uuid.uuid4(), name="Oh boy")
-            t3 = Track(id=uuid.uuid4(), name="Ze fox")
+            t1 = Track(name="Never Hit Bro")
+            t2 = Track(name="Oh boy")
+            t3 = Track(name="Ze fox")
             db.session.add_all([t1, t2, t3])
             db.session.commit()
         print("Database seeded!")
--- models.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_utils import UUIDType
-
-db = SQLAlchemy()
-
-class Track(db.Model):
-    id = db.Column(UUIDType(binary=False), primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
